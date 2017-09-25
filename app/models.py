7358eb05774d29f36b510ca4f84bdd2cb2230fe5
lixiaohan10 <lixiaohan@jd.com>
2017-09-25 20:36:50 +0800

    新增Article类

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article (models.Model):
+    title = models.CharField(max_length=100)
+    date_time = models.DateField(auto_now_add=True)
+    content = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date_time']
